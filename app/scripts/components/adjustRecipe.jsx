c8b2773e6467431618df0b3561ba0d2cb442deae
Luis Rene Onate <lonate12@gmail.com>
2016-11-10 16:07:20 -0500

    Added adjusted_quantity field to recipe, updated adjustment calculator.

--- app/scripts/components/adjustRecipe.jsx
+++ app/scripts/components/adjustRecipe.jsx
@@ -2,20 +2,27 @@ var React = require('react');
 
 var AdjustRecipeForm = React.createClass({
   getInitialState: function(){
+    console.log(this.props.recipe);
     return{
-      qty: this.props.recipe.get('quantity')
+      qty: this.props.recipe.get('adjusted_quantity')
     };
   },
   componentWillReceiveProps: function(nextProps){
-    this.setState({qty: nextProps.recipe.get('quantity')});
+    this.setState({qty: nextProps.recipe.get('adjusted_quantity')});
   },
   handleQty: function(e){
+    console.log('fired');
     this.setState({qty: e.target.value});
     this.props.adjustQtys(e.target.value);
   },
+  handleSubmit: function(e){
+    e.preventDefault();
+    this.props.adjustQtys(this.state.qty);
+  },
   render: function(){
+    console.log(this.state.qty);
     return(
-      <form className="form-inline well">
+      <form onSubmit={this.handleSubmit} className="form-inline well">
         <div className="form-group">
           How many {this.props.recipe.get('servings_unit')} would you like? <input onChange={this.handleQty} type="text" value={this.state.qty} />
         </div>
@@ -26,22 +33,48 @@ var AdjustRecipeForm = React.createClass({
 
 var IngredientsList = React.createClass({
   render: function(){
+    var factor = this.props.factor;
+
+    if(this.props.ingredients){
+      var ingredients = this.props.ingredients.map(function(ingredient){
+        var newAmount = ingredient.get('quantity') * factor;
+        var amount = parseInt(newAmount) === newAmount ? newAmount : newAmount.toFixed(2);
+
+        return(
+          <li key={ingredient.cid} className="list-group-item">
+            {amount} {ingredient.get('units')} {ingredient.get('name')}
+          </li>
+        );
+      });
+    }
     return(
-      <h1>Ingredients</h1>
+      <ul className="list-group">
+        {ingredients ? ingredients : null}
+      </ul>
     );
   }
 });
 
 var AdjustRecipeContainer = React.createClass({
-  adjustQtys: function(adjustmentValue){
-    console.log(adjustmentValue);
+  getInitialState: function(){
+    return{
+      factor: 1
+    };
+  },
+  adjustQtys: function(desiredServings){
+    var recipe = this.props.recipe;
+    var newFactor = (desiredServings/recipe.get('quantity')) || 1;
+
+    recipe.set({adjusted_quantity: desiredServings});
+
+    this.setState({factor: newFactor});
   },
   render: function(){
     return(
       <div>
         <AdjustRecipeForm recipe={this.props.recipe} adjustQtys={this.adjustQtys}/>
         <p className="lead">Ingredients</p>
-        <IngredientsList />
+        <IngredientsList factor={this.state.factor} ingredients={this.props.recipe.get('ingredients')}/>
       </div>
     );
   }
